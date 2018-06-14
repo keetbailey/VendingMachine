2b4acc7df4b062afd0a8a1e400f9376d53de30a2
John Fulton <john@TechElevator.com>
2018-06-14 05:41:25 -0400

    initial commit

---
+++ c#-mini-capstone/Capstone/Classes/UserInterface.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Classes
+{
+    public class UserInterface
+    {
+        private VendingMachine vendingMachine;
+
+        public UserInterface(VendingMachine vendingMachine)
+        {
+
+        }
+
+        public void RunInterface()
+        {
+            bool done = false;
+            while (!done)
+            {
+                Console.WriteLine("This is the UserInterface");
+                Console.ReadLine();
+            }
+
+        }
+
+    }
+}
