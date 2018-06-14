89c3e31f8e10f5dc26cf7327675e2f8c7d4fe5d1
Keet.Bailey <Keet.Bailey@gmail.com>
2018-06-14 12:40:28 -0400

    VendingMachineItemClass/Tests

--- c#-mini-capstone/Capstone/Classes/VendingMachineItem.cs
+++ c#-mini-capstone/Capstone/Classes/VendingMachineItem.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace Capstone.Classes
 {
-    class VendingMachineItem
+    public class VendingMachineItem
     {
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+        public string Type { get; private set; }
+        public string Message { get; private set; }
+        public int Quantity { get; private set; }
+
+        public VendingMachineItem(string name, decimal price, string type)
+        {
+            Name = name;
+            Price = price;
+            Type = type;
+            Quantity = 5;
+            Message = SetMessage(Type);
+        }
+
+        public string SetMessage(string type)
+        {
+            if (type == "Chip")
+            {
+                return "Crunch Crunch, Yum!";
+
+            }
+            else if (type == "Candy")
+            {
+                return "Munch Munch, Yum!";
+            }
+            else if (type == "Drink")
+            {
+                return "Glug Glug, Yum!";
+            }
+            else
+            {
+                return "Chew Chew, Yum!";
+            }
+        }
     }
 }
---
+++ c#-mini-capstone/CapstoneTests/VendingMachinesItemTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Classes;
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class VendingMachinesItemTest
+    {
+        VendingMachineItem item;
+
+        [TestMethod]
+        public void TestName()
+        {
+            item = new VendingMachineItem("Chips", 1.50M, "Chip");
+            Assert.AreEqual("Chips", item.Name);
+        }
+
+        [TestMethod]
+        public void TestPrice()
+        {
+            item = new VendingMachineItem("Chips", 1.50M, "Chip");
+            Assert.AreEqual(1.50M, item.Price);
+        }
+
+        [TestMethod]
+        public void TestType()
+        {
+            item = new VendingMachineItem("Chips", 1.50M, "Chip");
+            Assert.AreEqual("Chip", item.Type);
+        }
+
+        [TestMethod]
+        public void TestMessage()
+        {
+            item = new VendingMachineItem("Chips", 1.50M, "Chip");
+            Assert.AreEqual("Crunch Crunch, Yum!", item.SetMessage("Chip"));
+        }
+
+        [TestMethod]
+        public void TestMessage2()
+        {
+            item = new VendingMachineItem("Chips", 1.50M, "Chip");
+            Assert.AreEqual("Munch Munch, Yum!", item.SetMessage("Candy"));
+        }
+
+        [TestMethod]
+        public void TestMessage3()
+        {
+            item = new VendingMachineItem("Chips", 1.50M, "Chip");
+            Assert.AreEqual("Glug Glug, Yum!", item.SetMessage("Drink"));
+        }
+
+        [TestMethod]
+        public void TestMessage4()
+        {
+            item = new VendingMachineItem("Chips", 1.50M, "Chip");
+            Assert.AreEqual("Chew Chew, Yum!", item.SetMessage("Gum"));
+        }
+
+        [TestMethod]
+        public void TestQuanity()
+        {
+            item = new VendingMachineItem("Chips", 1.50M, "Chip");
+            Assert.AreEqual(5, item.Quantity);
+        }
+    }
+}
